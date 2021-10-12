c2886f3a21d4dd41cfe7be79247b9fd90865c785
07-Raqib <91867711+07-Raqib@users.noreply.github.com>
2021-10-12 13:59:08 +0800

    Added game manager and coin logic [collision]]

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public int score = 0;
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void IncreaseScoreBy(int score)
+    {
+        this.score += score;
+    }
+}
