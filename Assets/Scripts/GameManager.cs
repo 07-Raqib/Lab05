f46e5d3d460faf86fe493f5fdda909b608e9f10b
07-Raqib <91867711+07-Raqib@users.noreply.github.com>
2021-10-12 14:02:09 +0800

    Added UI score

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
     public int score = 0;
+
+    [Header("UI")]
+    public Text txt_score;
     void Awake()
     {
         if(instance == null)
@@ -20,8 +24,10 @@ public class GameManager : MonoBehaviour
     {
         
     }
+
     public void IncreaseScoreBy(int score)
     {
         this.score += score;
+        txt_score.text = "SCORE: " + this.score.ToString("N0");
     }
 }
