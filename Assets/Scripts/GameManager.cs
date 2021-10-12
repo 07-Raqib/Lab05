02f1e52ba395dc0c8a9fe6395ad1f412fab65736
07-Raqib <91867711+07-Raqib@users.noreply.github.com>
2021-10-12 14:15:16 +0800

    Updated GameManager; Implement timer logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
@@ -8,9 +9,14 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
 
     public int score = 0;
+    public int totalCoins;
+    public float timeLeft;
+    public int timeRemaining;
 
+    public float timerValue;
     [Header("UI")]
     public Text txt_score;
+    public Text txt_timer;
     void Awake()
     {
         if(instance == null)
@@ -22,7 +28,23 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeLeft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeLeft % 60);
+
+        txt_timer.text = "Timer : " + timeRemaining.ToString();
+
+
+        if(score == totalCoins)
+        {
+            if(timeLeft <= timerValue)
+            {
+                SceneManager.LoadScene("GameWinScene");
+            }
+        }
+        else if (timeLeft <= 0)
+        {
+            SceneManager.LoadScene("GameLoseScene");
+        }
     }
 
     public void IncreaseScoreBy(int score)
